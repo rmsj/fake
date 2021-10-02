86313e10dd7bdacf500f916f3640710309a6186a
Ronaldo <ronaldomuniz@gmail.com>
2021-10-03 12:45:11 +1300

    Add test for DNA sequence generation

---
+++ dna_test.go
@@ -0,0 +1,72 @@
+package fake_test
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/rmsj/fake"
+	"github.com/rmsj/fake/tests"
+)
+
+func TestDNASequence(t *testing.T) {
+
+	f, err := fake.New()
+
+	if err != nil {
+		t.Fatal("error creating fake instance")
+	}
+
+	tt := []struct {
+		name         string
+		sequenceSize int
+	}{
+		{"Too small DNA Sequence", 99},
+		{"Short DNA Sequence", 1000},
+		{"Medium DNA Sequence", 10000},
+		{"Long (ish) text", 65535},
+	}
+
+	t.Log("Given the need to create generate fake 'real text'")
+	{
+		for testID, test := range tt {
+			// start with test 1
+			testID++
+			tf := func(t *testing.T) {
+				t.Logf("\tTest %d:\tWhen creating a DNA Sequence %d characters.", testID, test.sequenceSize)
+				{
+					dna, err := f.DNASequence(test.sequenceSize)
+					if err == nil && test.sequenceSize < 100 {
+						t.Fatalf(tests.Failed("\t", "Test %d:\tShould not create DNA sequences smaller than 100 characters. Created one with %d characters"), testID, test.sequenceSize)
+					} else if err != nil {
+						t.Fatalf(tests.Success("\t", "Test %d:\tShould not create a DNA sequence smaller than 100 characters: %v"), testID, err)
+					}
+
+					if len(dna) == 0 {
+						t.Fatalf(tests.Failed("\t", "Test %d:\tShould have created a valid DNA sequence but it's empty: %s"), testID, dna)
+					}
+					t.Logf(tests.Success("\t", "Test %d:\tShould have create a valid DNA Sequence"), testID)
+
+					if len(dna) != test.sequenceSize {
+						t.Fatalf(tests.Failed("\t", "Test %d:\tShould have created a valid DNA sequence with %d characters length: %d"), testID, test.sequenceSize, len(dna))
+					}
+					t.Logf(tests.Success("\t", "Test %d:\tShould have create a valid DNA Sequence with %d characters length"), testID, test.sequenceSize)
+
+					// clear the text off some stuff
+					chars := []string{"A", "C", "G", "T"}
+					reg := strings.Join(chars, "")
+
+					// last treatments - removing special characters, etc.
+					re := regexp.MustCompile("[" + reg + "]+")
+					result := re.ReplaceAllString(dna, "")
+					if len(result) > 0 {
+						t.Fatalf(tests.Failed("\t", "Test %d:\tShould have created a valid DNA sequence only using characters A, C, G and T: %s"), testID, result)
+					}
+				}
+			}
+
+			t.Run(test.name, tf)
+
+		}
+	}
+}
